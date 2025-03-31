1e71a9d33f6fbe68bbf691f080da1f6a22f4b28a
Carlos Rios <lawlercarlospatricio@gmail.com>
2025-03-31 18:25:46 -0300

    Add files via upload

--- ColegioStub/src/colegiostub/AlumnoRepository2.java
+++ ColegioStub/src/colegiostub/AlumnoRepository2.java
@@ -17,22 +17,20 @@ public class AlumnoRepository2 extends AlumnoRepository {
     public Connection conn;
     public AlumnoRepository2(Connection conn) {
         super(conn);
+        this.conn=conn;
     }
-
 
     public void remove(Alumno alumno) {
         if(alumno==null) return;
-        //try (PreparedStatement ps=conn.prepareStatement("delete from alumnos where id=?")){
-        try (PreparedStatement ps=conn
-                .prepareStatement("delete from alumnos")){
+        try (PreparedStatement ps=conn.prepareStatement("delete from alumnos where id=?")){
+        //try (PreparedStatement ps=conn
+                //.prepareStatement("delete from alumnos")){
+                //.prepareStatement("delete from cursos")){
             ps.setInt(1, alumno.getId());
             ps.execute();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }    
-    
-    
-    
     
 }
--- ColegioStub/src/colegiostub/ColegioStub.java
+++ ColegioStub/src/colegiostub/ColegioStub.java
@@ -4,21 +4,28 @@
  */
 package colegiostub;
 
+import ar.org.centro8.curso.java.connectors.Connector;
+import ar.org.centro8.curso.java.entities.Alumno;
 import ar.org.centro8.curso.java.gui.ColegioApp;
 
-/**
- *
- * @author centro8
- */
-public class ColegioStub extends ColegioApp{
-
-    /**
-     * @param args the command line arguments
-     */
+public class ColegioStub{
+    
+    
+    //private static AlumnoRepository2 ar=new AlumnoRepository2(Connector.getConnection());
     public static void main(String[] args) {
-        ColegioApp.main(args);
+        
+        ColegioApp c=new ColegioApp();
+        FormAlumnos2 a=new FormAlumnos2();
+        c.desktop.add(a);
+        a.setVisible(true);
+        c.setVisible(true);
+        
+        //ar.remove(new Alumno(1, "", "", 1, 1));
+        
+        
+        
     }
     
-    
+   
     
 }
