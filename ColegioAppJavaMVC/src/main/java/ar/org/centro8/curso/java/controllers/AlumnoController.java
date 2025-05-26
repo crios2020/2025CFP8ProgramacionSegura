88d47aaafac39937e7ad7caa6c0321e84cacfd57
carlos <lawlercarlospatricio@gmail.com>
2025-05-26 17:15:26 -0300

    JPA Java MVC

--- ColegioAppJavaMVC/src/main/java/ar/org/centro8/curso/java/controllers/AlumnoController.java
+++ ColegioAppJavaMVC/src/main/java/ar/org/centro8/curso/java/controllers/AlumnoController.java
@@ -1,5 +1,8 @@
 package ar.org.centro8.curso.java.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +12,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import ar.org.centro8.curso.java.entities.Alumno;
 import ar.org.centro8.curso.java.repositories.AlumnoRepository;
+import ar.org.centro8.curso.java.repositories.AlumnoRepository2;
 import ar.org.centro8.curso.java.repositories.CursoRepository;
+import ar.org.centro8.curso.java.repositories.CursoRepository2;
 
 @Controller
 public class AlumnoController{
     
     private String mensaje="Ingrese un nuevo alumno!";
-    private AlumnoRepository alumnoRepository=new AlumnoRepository();
-    private CursoRepository cursoRepository=new CursoRepository();
+    //private AlumnoRepository alumnoRepository=new AlumnoRepository();
+    //private CursoRepository cursoRepository=new CursoRepository();
+
+    @Autowired
+    private AlumnoRepository2 alumnoRepository;
+      
+    @Autowired
+    private CursoRepository2 cursoRepository;
 
     @GetMapping("/alumnos")
     public String getAlumnos(Model model, @RequestParam(name="buscar", defaultValue = "") String buscar){
@@ -24,8 +35,16 @@ public class AlumnoController{
         alumno.setEdad(18);
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("alumno", alumno);
-        model.addAttribute("cursos", cursoRepository.getAll());
-        model.addAttribute("likeApellido", alumnoRepository.getLikeApellido(buscar));
+        model.addAttribute("cursos", cursoRepository.findAll());
+        model.addAttribute("likeApellido", 
+                            ((List<Alumno>)alumnoRepository
+                                .findAll())
+                                .stream()
+                                .filter(a->a
+                                            .getApellido()
+                                            .toLowerCase()
+                                            .contains(buscar))
+                                .toList());
         return "alumnos";
     }
 
--- ColegioAppJavaMVC/src/main/java/ar/org/centro8/curso/java/controllers/CursoController.java
+++ ColegioAppJavaMVC/src/main/java/ar/org/centro8/curso/java/controllers/CursoController.java
@@ -1,8 +1,10 @@
 package ar.org.centro8.curso.java.controllers;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.catalina.connector.Request;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,16 @@ import ar.org.centro8.curso.java.entities.Curso;
 import ar.org.centro8.curso.java.enums.Dia;
 import ar.org.centro8.curso.java.enums.Turno;
 import ar.org.centro8.curso.java.repositories.CursoRepository;
+import ar.org.centro8.curso.java.repositories.CursoRepository2;
 
 @Controller
 public class CursoController {
     
     private String mensaje="Ingrese un nuevo curso!";
-    private CursoRepository cursoRepository=new CursoRepository();
+    //private CursoRepository cursoRepository=new CursoRepository();
+
+    @Autowired
+    private CursoRepository2 cursoRepository;
 
     @GetMapping("/cursos")
     public String getCursos(Model model, @RequestParam(name="buscar", defaultValue = "") String buscar){
@@ -27,8 +33,15 @@ public class CursoController {
         model.addAttribute("turnos", Arrays.asList(Turno.values()));
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("curso", new Curso());
-        //model.addAttribute("all", cursoRepository.getAll());
-        model.addAttribute("likeTitulo", cursoRepository.getLikeTitulo(buscar));
+        //model.addAttribute("all", cursoRepository.findAll());
+        model.addAttribute("likeTitulo", 
+            ((List<Curso>)cursoRepository.findAll())
+                    .stream()
+                    .filter(c->c
+                                    .getTitulo()
+                                    .toLowerCase()
+                                    .contains(buscar))
+                    .toList());
         return "cursos";
     }
 
